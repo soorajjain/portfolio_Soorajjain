9bdafdaff7a6c49760db3dfe250431488dcad2f7
soorajain <soorajjain51@gmail.com>
2024-08-13 19:46:00 +0530

    resume Update

--- src/pages/main/About.js
+++ src/pages/main/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import computer from "../../assets/hero-devices-sj.svg";
 import Button from "@mui/material/Button";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
-import myResume from "../../assets/pdf/";
+import myResume from "../../assets/pdf/soorajjain_python.pdf";
 
 function About() {
   return (
